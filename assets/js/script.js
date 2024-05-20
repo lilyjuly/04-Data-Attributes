8047a802c9e88e90b657d8aa883fdd4ddf5670a4
lily <lilyjulymm2009@gmail.com>
2024-05-20 20:42:15 +0630

    04-Data-Attributes

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+var container = document.querySelector(".container");
+
+container.addEventListener("click", function(event) {
+  var element = event.target;
+
+  if (element.matches(".box")) {
+    var state = element.getAttribute("data-state");
+
+    // 
+    if (state === "hidden") {
+        // 
+        element.textContent = element.dataset.number;
+        // 
+        element.dataset.state = "visble";
+    }else {
+        // 
+        element.textContent = "";
+        // 
+        element.setAttribute("data-state", "hidden")
+    }
+  }
+});
